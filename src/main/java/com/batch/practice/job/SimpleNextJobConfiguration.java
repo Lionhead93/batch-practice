f4989c2a2d7545f44df92e0e62328a2aa8d07f1e
Lionhead93 <lionhead115@naver.com>
2020-03-03 15:10:48 +0900

    practice step

--- src/main/java/com/batch/practice/job/SimpleNextJobConfiguration.java
+++ src/main/java/com/batch/practice/job/SimpleNextJobConfiguration.java
@@ -5,48 +5,54 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
-@RequiredArgsConstructor
 @Configuration
-public class SimpleJobConfiguration {
+@RequiredArgsConstructor
+public class SimpleNextJobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job simpleJob() {
-        return jobBuilderFactory.get("simpleJob")
-                .start(simpleStep1(null))
-                .next(simpleStep2(null))
+    public Job stepNextJob() {
+        return jobBuilderFactory.get("stepNextJob")
+                .start(step1())
+                .next(step2())
+                .next(step3())
                 .build();
     }
 
-    @Bean
-    @JobScope
-    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
-        return stepBuilderFactory.get("simpleStep1")
-                .tasklet(((contribution, chunkContext) -> {
-                    log.info(">>>>> This is Step1");
-                    log.info(">>>>> requestDate = {}", requestDate);
-                    return RepeatStatus.FINISHED;
-                }))
-                .build();
-    }
-
-    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
-        return stepBuilderFactory.get("simpleStep2")
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
                 .tasklet((contribution, chunkContext) -> {
-                    log.info(">>>>> This is Step2");
-                    log.info(">>>>> requestDate = {}", requestDate);
+                    log.info(">>>>> This is Step1");
                     return RepeatStatus.FINISHED;
                 })
                 .build();
     }
+
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step3");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+
 }
---
+++ src/main/java/com/batch/practice/job/StepNextConditionalJobConfiguration.java
@@ -0,0 +1,70 @@
+package com.batch.practice.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class StepNextConditionalJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job stepNextConditionalJob() {
+        return jobBuilderFactory.get("stepNextConditionalJob")
+                .start(conditionalJobStep1())
+                    .on("FAILED")
+                    .to(conditionalJobStep3())
+                    .on("*")
+                    .end()
+                .from(conditionalJobStep1())
+                    .on("*")
+                    .to(conditionalJobStep2())
+                    .next(conditionalJobStep3())
+                    .on("*")
+                    .end()
+                .end()
+                .build();
+
+    }
+
+    @Bean
+    public Step conditionalJobStep1() {
+        return stepBuilderFactory.get("conditionalJobStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is stepNextConditionalJob Step1");
+                    contribution.setExitStatus(ExitStatus.FAILED);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    public Step conditionalJobStep2() {
+        return stepBuilderFactory.get("conditionalJobStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is stepNextConditionalJob Step2");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    public Step conditionalJobStep3() {
+        return stepBuilderFactory.get("conditionalJobStep3")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is stepNextConditionalJob Step3");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+}
